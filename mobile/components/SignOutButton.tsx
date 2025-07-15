4a8deca146b5aa6c2f1cace1e882d4d533ec9785
Harneet <harneetsahi8@gmail.com>
2025-07-14 21:29:33 -0500

    add: sign out button

---
+++ mobile/components/SignOutButton.tsx
@@ -0,0 +1,28 @@
+import { useClerk } from "@clerk/clerk-expo";
+import * as Linking from "expo-linking";
+import { Text, TouchableOpacity } from "react-native";
+
+export const SignOutButton = () => {
+  const { signOut } = useClerk();
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      Linking.openURL(Linking.createURL("/"));
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+  return (
+    <TouchableOpacity
+      onPress={handleSignOut}
+      style={{
+        borderRadius: 20,
+        paddingVertical: 11,
+        paddingHorizontal: 20,
+        backgroundColor: "#fbc933ff",
+      }}
+    >
+      <Text>Sign out</Text>
+    </TouchableOpacity>
+  );
+};
