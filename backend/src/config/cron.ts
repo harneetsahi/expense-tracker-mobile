0312eb8194aff6af11ebafba71457d7b77562a26
Harneet <harneetsahi8@gmail.com>
2025-07-18 21:45:34 -0500

    update types

--- backend/src/config/cron.ts
+++ backend/src/config/cron.ts
@@ -6,7 +6,7 @@ const url = process.env.API_URL as string;
 
 const job = new CronJob("*/14 * * * *", function () {
   https
-    .get(url, (res) => {
+    .get(url, (res: any) => {
       if (res.statusCode === 200) console.log("Get request sent");
       else console.log("Get request failed", res.statusCode);
     })
