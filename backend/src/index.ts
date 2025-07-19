6f92dcdc463fb6f5d33da964306f717a0c7345ae
Harneet <harneetsahi8@gmail.com>
2025-07-18 21:14:18 -0500

    update: organized code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,24 @@
-import express from "express";
+import express, { Response } from "express";
 import dotenv from "dotenv";
-import { sql } from "./config/db";
+import { connectDB } from "./config/db";
 import rateLimiter from "./middleware/rateLimiter";
 import txrouter from "./routes/tx-routes";
+import job from "./config/cron";
 
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start();
+
 app.use(express.json());
+app.use(rateLimiter);
 
 const PORT = process.env.PORT || 3000;
 
-async function connectDB() {
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS transactions(
-      id SERIAL PRIMARY KEY,
-      user_id VARCHAR(255) NOT NULL,
-      title VARCHAR(255) NOT NULL,
-      amount DECIMAL(10,2) NOT NULL,
-      category VARCHAR(255) NOT NULL,
-      created_at DATE NOT NULL DEFAULT CURRENT_DATE)`;
-
-    console.log("DB connected successfully");
-  } catch (error) {
-    console.error("Failed to connect to DB ", error);
-    process.exit(1);
-  }
-}
-
-app.use(rateLimiter);
+app.get("/api/health", (_, res: Response) => {
+  res.status(200).json({ status: "ok" });
+});
 
 app.use("/api/transactions", txrouter);
 
