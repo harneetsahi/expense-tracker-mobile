3822ceca1d17a3e4802feebce1c9a75ed452fb9d
Harneet <harneetsahi8@gmail.com>
2025-07-21 17:14:09 -0500

    add: status bar for visibility

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,6 +3,7 @@ import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Slot } from "expo-router";
 
 import SafeScreen from "@/components/SafeScreen";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
@@ -10,6 +11,7 @@ export default function RootLayout() {
       <SafeScreen>
         <Slot />
       </SafeScreen>
+      <StatusBar style="dark" />
     </ClerkProvider>
   );
 }
